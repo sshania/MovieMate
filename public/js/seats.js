872bec1f7aead8f0e7725afce932098389bde9b5
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2024-11-12 04:34:03 +0700

    feat: add logic selected seats

---
+++ public/js/seats.js
@@ -0,0 +1,51 @@
+const container = document.querySelector(".main-container");
+const takenContainer = document.querySelector(".taken");
+const inputContainer = document.querySelector("input[name='seat-name']");
+const priceContainer = document.getElementById("price");
+const inputPriceContainer = document.querySelector("input[name='sub-total']");
+const seatContainer = document.getElementById("seat-count");
+const inputSeatContainer = document.querySelector("input[name='seat-total']");
+
+const selectedSeats = [];
+let priceCount = 0;
+let seatCount = 0;
+
+container.addEventListener("click", (e) => {
+    if (e.target.classList.contains("seat")) {
+        e.target.classList.toggle("select");
+        const seatChosen = e.target.textContent;
+        const price = 60000;
+        const n = selectedSeats.indexOf(seatChosen);
+
+        if (n === -1){
+            selectedSeats.push(seatChosen);
+            priceCount += price;
+            seatCount += 1;
+        } else{
+            const seatIndex = selectedSeats.indexOf(seatChosen);
+            selectedSeats.splice(seatIndex, 1);
+            priceCount -= price;
+            seatCount -= 1;
+        }
+
+        seatChosenIdx();
+        totalPrice();
+        totalSeats();
+    }
+});
+
+function seatChosenIdx(){
+    const seatsText = selectedSeats.length > 0 ? selectedSeats.join(" ") : "";
+    takenContainer.textContent = seatsText;
+    inputContainer.value = seatsText;
+}
+
+function totalPrice(){
+    priceContainer.textContent = priceCount.toLocaleString();
+    inputPriceContainer.value = priceCount.toLocaleString();
+}
+
+function totalSeats(){
+    seatContainer.textContent = seatCount.toLocaleString();
+    inputSeatContainer.value = seatCount.toLocaleString();
+}
