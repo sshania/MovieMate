1f68b2c01c2336084aba753b2910bed837667bc5
VinAhmad <kev.af18@gmail.com>
2024-11-25 16:15:56 +0700

    feat: add all database migration

---
+++ database/migrations/2024_11_25_082230_create_studios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('studios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cinema_id');
+            $table->string('studio_number');
+            $table->integer('total_seats');
+            $table->timestamps();
+
+            $table->foreign('cinema_id')->references('id')->on('cinemas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('studios');
+    }
+};
