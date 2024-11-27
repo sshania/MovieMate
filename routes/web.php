b6946f0c5d2bf8329b39f313ae144823e812c4d6
S <145682660+sshania@users.noreply.github.com>
2024-11-27 19:10:25 +0700

    feat: register page

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/auth', [AuthController::class, 'index']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::get('/auth/logout', [AuthController::class, 'logout']);
 
+Route::get('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/create', [AuthController::class, 'create']);
