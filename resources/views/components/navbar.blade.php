aa2ba3e092208ac4f556c39bbf30172de206df68
S <145682660+sshania@users.noreply.github.com>
2024-12-02 23:16:58 +0700

    feat: add picture homepage

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,13 +1,13 @@
 <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
 <nav class="navbar">
     <div class="logo">
-        <a href="">
+        <a href="/">
             <img src="../assets/LogoMain.png" alt="Logo" style="margin: 0%">
         </a>
     </div>
     <div class="menu">
-        <a href="#">NOW SHOWING</a>
-        <a href="#">UPCOMING</a>
+        <a href="/">NOW SHOWING</a>
+        <a href="/">UPCOMING</a>
         <a href="#">THEATER</a>
         <a href="#">BOOKING</a>
         {{-- <a href="#">Lol</a> --}}
---
+++ resources/views/main/detail.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts/home-layout')
+
+@section('title', 'MovieMate | HomePage')
+
+@section('content')
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+
+@endsection
