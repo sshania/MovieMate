bd1ebd4ff98150530569773d41d98c6c055a20bc
S <145682660+sshania@users.noreply.github.com>
2024-12-04 22:48:37 +0700

    feat: text for profile

--- resources/views/main/profile.blade.php
+++ resources/views/main/profile.blade.php
@@ -6,11 +6,16 @@
         <div class="content">
             <img class="image-container" src="{{ asset('../assets/bioskop.jpeg') }}" alt="pic">
             <img class="image-logo" src="{{ asset('../assets/Logo-White.png') }}" alt="MovieMate Logo">
-            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget lorem consequat,
-                rutrum mi ut, molestie nisl. Mauris posuere arcu metus pharetra pretium. Vivamus id risus lorem.
-                Maecenas id quam orci. Mauris nec eu arcu dictumismod in nec dui. Praesent vulputate tempus ipsum nullam.
-                Morbi sagittis ipsum sed tristique. Donec interdum malesuada odio nec vulputate vehicula. Nulla pharetra
-                iaculis felis. Quisque quam. In augue nisi. Pellentesque euismod egestas nisi non malesuada.</p>
+            <p class="description">
+                Moviemate is your ultimate movie companion, dedicated to making your movie-going experience as
+                seamless and enjoyable as possible. Our platform offers easy access to the latest films, showtimes,
+                and cinemas, allowing you to quickly book tickets with just a few clicks. Whether you’re planning a
+                night out with friends, a family movie night, or looking for a special screening, Moviemate ensures
+                you have all the information you need to make the best choice. With exclusive discounts, transparent
+                pricing, and a user-friendly interface, we make sure you can focus on what truly matters—enjoying your
+                favorite films. Join our growing community of movie lovers and experience the convenience and excitement
+                that Moviemate brings to every movie night. Let us take care of your ticketing needs, so you can sit back,
+                relax, and enjoy the show!</p>
         </div>
     </div>
 
