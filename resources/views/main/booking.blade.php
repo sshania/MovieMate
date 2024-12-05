ff4f62fcba7547d6c29376f4134e709ff4e95171
Austinatan22 <austinatan22@gmail.com>
2024-12-06 03:28:38 +0700

    Merge remote-tracking branch 'origin/feature/home-page'

--- resources/views/main/booking.blade.php
+++ resources/views/main/booking.blade.php
@@ -6,17 +6,6 @@
     <link rel="stylesheet" href="{{asset('css/home.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
 
-<<<<<<< HEAD
-    <div class="now-showing-container">
-        <h1 class="title-booking" id="now-showing"><span>Choose</span> A Movie</h1>
-        <div class="movies">
-            @foreach ($movieNow as $mn)
-            <a href="/movies/{{ $mn->id }}">
-                <div class="movie-container">
-                    <div class="movie-card">
-                        <div class="movie">
-                            <image src="{{ asset('poster/' . $mn->movie_images) }}" alt="{{$mn->title}}" class="movie-image"></image>
-=======
     <div class="now-playing-container" style="margin: 5% auto">
         <h1 class="title-booking" id="now-playing">
             <span class="choose">Choose</span> <span class="movie">A Movie</span>
@@ -37,7 +26,6 @@
                                     {{ $mn->rating }}
                                 </span>
                             </div>
->>>>>>> origin/feature/home-page
                         </div>
                     </a>
                 @endforeach
