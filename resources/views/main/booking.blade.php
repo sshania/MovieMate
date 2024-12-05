506d7fa99ac630a3f0b1d7d06e2c28781d07d778
Austinatan22 <austinatan22@gmail.com>
2024-12-06 03:17:43 +0700

    feat: unified design from now showing

--- resources/views/main/booking.blade.php
+++ resources/views/main/booking.blade.php
@@ -1,34 +1,36 @@
 @extends('layouts/home-layout')
 
-@section('title', 'MovieMate | Booking')
+@section('title', 'Booking')
 
 @section('content')
-    <link rel="stylesheet" href="{{asset('css/booking.css')}}">
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
 
-    <div class="now-playing-container">
-        <h1 class="title-booking" id="now-playing"><span>Choose</span> A Movie</h1>
-        <div class="movies">
-            @foreach ($movieNow as $mn)
-            <a href="/movies/{{ $mn->id }}">
-                <div class="movie-container">
-                    <div class="movie-card">
-                        <div class="movie">
-                            <image src="{{ asset('poster/' . $mn->movie_images) }}" alt="{{$mn->title}}" class="movie-image"></image>
+    <div class="now-playing-container" style="margin: 5% auto">
+        <h1 class="title-booking" id="now-playing">
+            <span class="choose">Choose</span> <span class="movie">A Movie</span>
+        </h1>
+        <div class="movies-container">
+            <div class="movies">
+                @foreach ($movieNow as $mn)
+                    <a href="/movies/{{ $mn->id }}" class="movie-link">
+                        <div class="movie-container">
+                            <div class="movie-card">
+                                <div class="movie">
+                                    <image src="{{ asset('poster/' . $mn->movie_images) }}" alt="{{$mn->title}}" class="movie-image"></image>
+                                </div>
+                            </div>
+                            <div class="movie-text">
+                                <h3 class="movie-title">{{$mn->title}}</h3>
+                                <span class="movie-rating {{ $mn->rating }}">
+                                    {{ $mn->rating }}
+                                </span>
+                            </div>
                         </div>
-                    </div>
-
-                    <div class="movie-text">
-                        <h3 class="movie-title">{{$mn->title}}</h3>
-                        <span class="movie-rating {{ $mn->rating }}">
-                            {{ $mn->rating }}
-                        </span>
-                    </div>
-                </div>
-            </a>
-            @endforeach
+                    </a>
+                @endforeach
+            </div>
+            {{ $movieNow->links() }}
         </div>
-        {{ $movieNow->links() }}
     </div>
-
 @endsection
